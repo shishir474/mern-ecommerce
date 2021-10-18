5d6c3690f5a66c080990da9f9350518c09fdc183
Shishir Singh <shishirsinghvaranasi@gmail.com>
2021-10-18 12:36:23 -0700

    sub categories Rest api implemented

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -24,6 +24,30 @@ exports.createCategory = (req,res)=>{
     })
 }
 
+function getCategory(categories, parentId=null){
+
+    let categoryList = [];
+    let category;
+    if (parentId==null){
+        category = categories.filter(cat => cat.parentId == undefined);
+    }
+    else{
+        category = categories.filter(cat => cat.parentId == parentId);
+    }
+
+    // looping over all the category
+    for (cate of category){
+      categoryList.push({
+          _id: cate._id,
+          name: cate.name,
+          slug:cate.slug,
+          children: getCategory(categories, cate._id)
+      })
+    }
+
+    return categoryList;
+}
+
 
 exports.getCategory = (req,res)=>{
     Category.find({})
@@ -32,7 +56,9 @@ exports.getCategory = (req,res)=>{
             return res.status(400).json({error});
         }
         if (categories){
-            return res.status(200).json({categories});
+            // recursive function to get the sub categories
+            categoryList = getCategory(categories);
+            return res.status(200).json({categoryList});
         }
  
     })
